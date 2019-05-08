1aa3efdc34030e5ea636b86aa8a5d6a2af66ef0b
Gavyn Riebau <gavyn.riebau@gmail.com>
2019-05-08 20:00:11 +0800

    Add MTU fix for some google sites not working and reject all ip6 traffic

--- server/nat-setup.sh
+++ server/nat-setup.sh
@@ -4,7 +4,5 @@ iptables -A FORWARD -i wg0 -j ACCEPT
 iptables -A FORWARD -i ens4 -j ACCEPT
 iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
-ip6tables -A FORWARD -i wg0 -j ACCEPT
-ip6tables -A FORWARD -i ens4 -j ACCEPT
-ip6tables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
-ip6tables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+ip6tables -I FORWARD -i wg0 -j REJECT
+ip6tables -I FORWARD -i ens4 -j REJECT
--- server/nat-teardown.sh
+++ server/nat-teardown.sh
@@ -4,7 +4,5 @@ iptables -D FORWARD -i wg0 -j ACCEPT
 iptables -D FORWARD -i ens4 -j ACCEPT
 iptables -t nat -D POSTROUTING -o wg0 -j MASQUERADE
 iptables -t nat -D POSTROUTING -o ens4 -j MASQUERADE
-ip6tables -D FORWARD -i wg0 -j ACCEPT
-ip6tables -D FORWARD -i ens4 -j ACCEPT
-ip6tables -t nat -D POSTROUTING -o wg0 -j MASQUERADE
-ip6tables -t nat -D POSTROUTING -o ens4 -j MASQUERADE
+ip6tables -D FORWARD -i wg0 -j REJECT
+ip6tables -D FORWARD -i ens4 -j REJECT
